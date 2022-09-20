a60c4f0199f29f6caf4728f8d33961c3cce0c0cf
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2022-09-19 23:40:20 -0300

    por equanto

--- src/components/perfil/perfil.js
+++ src/components/perfil/perfil.js
@@ -7,6 +7,14 @@ import linkedin from "./../../assets/icons/linkedin.svg";
 import git from "./../../assets/icons/gitHub.svg";
 import insta from "./../../assets/icons/instagram.svg";
 
+const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+};
+
+const openEmai = url => {
+    window.location.href = url;
+};
+
 export default function Perfil() {
     return (
         <div className="content">
@@ -15,15 +23,20 @@ export default function Perfil() {
             <div className="he">(He/him)</div>
             <div className="work-name">Software Developer</div>
             <div className="contact-txt">CONNECT WITH ME</div>
-            
+
             <div className="contact-icons-conteiner">
-                <img src={email} className="contact-icons" />
 
-                <img src={linkedin} className="contact-icons" />
+                {/* <a className="contact-icons" href="mailto:michelfc42@gmail.com">
+                    <img src={email} className="contact-icons" />
+                </a> */}
+                
+                <img src={email} href="mailto:michelfc42@gmail.com"  className="contact-icons" />
 
-                <img src={git} className="contact-icons" />
+                <img src={linkedin} onClick={() => openInNewTab('https://www.linkedin.com/in/jean-michel-fernandes-carvalho-5682a8153/?locale=en_US')} className="contact-icons" />
 
-                <img src={insta} className="contact-icons" />
+                <img src={git} onClick={() => openInNewTab('https://github.com/Gl1tch42')} className="contact-icons" />
+
+                <img src={insta} onClick={() => openInNewTab('https://www.instagram.com/jeanmichelfc/')} className="contact-icons" />
             </div>
         </div>
     )
